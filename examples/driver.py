ab4fd9c9813c52bdef1f511d888cef5be44f8eae
Dammmmilare <josh.oyed@gmail.com>
2026-01-19 10:47:08 -0700

    Changes made

--- examples/driver.py
+++ examples/driver.py
@@ -4,9 +4,9 @@ square_ones,
 )
 def main():
 # test creating square array of ones
-A_np = np.ones((3,3))
-A = square_ones(3)
-print(f'A_np:\n{A_np}')
-print(f'A:\n{A}')
+    A_np = np.ones((3,3))
+    A = square_ones(3)
+    print(f'A_np:\n{A_np}')
+    print(f'A:\n{A}')
 if __name__ == '__main__':
-main()
+    main()
