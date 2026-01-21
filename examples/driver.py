2ec75b7b8c94655a62dddb2b351acd676f230886
Dammmmilare <josh.oyed@gmail.com>
2026-01-21 11:26:10 -0700

    examples/driver.py

--- examples/driver.py
+++ examples/driver.py
@@ -10,3 +10,17 @@ def main():
     print(f'A:\n{A}')
 if __name__ == '__main__':
     main()
+
+def Part_B():
+# Creating an array of ones with 3 rows and 5 columns
+    B_np = np.ones((3, 5))
+    B = square_ones(3)
+    print(f'B_np:\n{B_np}')
+    print(f'B:\n{B}')
+# Creating an array of NaN with 3 rows and 5 columns
+    B_np = np.NaN((3, 5))
+    B = square_NaN(3)
+    print(f'B_np:\n{B_np}')
+    print(f'B:\n{B}')
+if __name__ == '__main__':
+    PartB()
