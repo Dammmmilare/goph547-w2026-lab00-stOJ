c3ef48944cece39759ace090dd036847de52cc00
Dammmmilare <josh.oyed@gmail.com>
2026-01-23 10:19:41 -0700

    Latest updates for matplot question

--- examples/driver-0C.py
+++ examples/driver-0C.py
@@ -1,3 +1,5 @@
+from tkinter import Image
+from matplotlib import image
 import numpy as np
 import matplotlib.pyplot as plt
 from goph547lab00.arrays import (
@@ -6,7 +8,9 @@ square_ones,
 
 # matplotlib - plt work.
 def Part_C():
-    
+    # loading image
+    image = np.asarray(Image.open('examples/rock_canyon.jpg'))
+    plt.imshow(image)
 
 if __name__ == '__main__':
     Part_C()
