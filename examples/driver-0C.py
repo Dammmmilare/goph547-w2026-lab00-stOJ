c80857ba62a8ea5c7c1d7a5f0e89313d466a8985
Dammmmilare <josh.oyed@gmail.com>
2026-01-23 10:53:27 -0700

    driver c updated

--- examples/driver-0C.py
+++ examples/driver-0C.py
@@ -1,16 +1,27 @@
-from tkinter import Image
-from matplotlib import image
+from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from goph547lab00.arrays import (
-square_ones,
-)
 
 # matplotlib - plt work.
+
 def Part_C():
     # loading image
-    image = np.asarray(Image.open('examples/rock_canyon.jpg'))
-    plt.imshow(image)
+    img = np.asarray(Image.open('examples/rock_canyon.jpg'))
+    plt.imshow(img)
+    plt.savefig('examples/rock_canyon.png')
+    plt.show()
 
-if __name__ == '__main__':
-    Part_C()
+    # Converting the image to greyscale
+    grey_scale = np.asarray(Image.open('examples/rock_canyon.jpg').convert("L"))
+    plt.imshow(grey_scale, cmap=('gray'))
+    plt.savefig('examples/rock_canyon_grey_scale version.png')
+    plt.show()
+
+    # Displaying a smaller grey scale image 
+    small_grey_scale = grey_scale[0:100, 0:100]
+    plt.imshow(small_grey_scale, cmap=('gray'))
+    plt.savefig('examples/small_rock_canyon_grey_scale version.png')
+    plt.show()
+    
+#if __name__ == '__main__':
+    #Part_C()
